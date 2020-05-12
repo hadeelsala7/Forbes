5aad054a406a5f9c7363812a95676e4f91d320c3
hadeel salah <hadeelsalah80@gmail.com>
2020-05-12 23:37:26 +0300

    Edits App.js file

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from './Components/LayoutComponent/Home/Home';
+
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
