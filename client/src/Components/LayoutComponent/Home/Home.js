eb6bfd3c257c271863018f5c9d7bda7cbf2415ba
hadeel salah <hadeelsalah80@gmail.com>
2020-05-12 23:36:07 +0300

    Create home page

---
+++ client/src/Components/LayoutComponent/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../../FixedComponent/MainHeader/Header';
+import Footer from '../../FixedComponent/MainFooter/Footer'
+import './home.css';
+
+export default function Home() {
+    return (
+        <div className="home">
+            <Header></Header>
+            <div className="try">
+            <h1>hi</h1>
+            </div>
+            <Footer></Footer>
+            
+        </div>
+    )
+}
